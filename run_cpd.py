fd668a01b8a18a4bbe5bb4f11ee04701b34ad3b0
Michele Vascellari <Michele.Vascellari@vtc.tu-freiberg.de>
2016-11-11 15:15:15 +0100

    Add validation against Fortran CPD

--- run_cpd.py
+++ run_cpd.py
@@ -1,21 +1,34 @@
 import cpd
+import pkp
 import matplotlib
 import matplotlib.pyplot as plt
 import logging
+import pandas as pd
 
 matplotlib.use('GTK3Agg')
 
-log_level = logging.INFO
+log_level = logging.WARNING
 
 logging.basicConfig(level=log_level)
 
 plt.style.use(['mystyle', 'mystyle-vega'])
+colors = plt.rcParams['axes.prop_cycle'].by_key()['color']
+
 
 if __name__ == '__main__':
     ua = {'C': 74.12, 'H': 4.96, 'O': 13.18, 'N': 1.45, 'S': 0}
     pa = {'FC': 57, 'VM': 43, 'Ash': 0, 'Moist': 0}
 
     operating_conditions = [[0, 1000], [0.01, 1000], [0.05, 1000]]
+
+    try:
+        results0 = pd.read_csv('CPD_Test.csv', index_col=0)
+    except FileNotFoundError:
+        c0 = pkp.cpd.CPD(ultimate_analysis=ua, proximate_analysis=pa,
+                         pressure=101325, name='Test')
+        c0.operating_conditions = operating_conditions
+        c0.set_parameters(dt=1e-5, dt_max=10)
+        results0 = c0.run()
 
     c = cpd.CPD(ultimate_analysis=ua, proximate_analysis=pa,
                 pressure=101325, name='Test')
@@ -33,18 +46,27 @@ if __name__ == '__main__':
     f = 1 - p
     g1 = 2 * f - delta
     g2 = 2 * (c - c[0])
-    ax1.plot(t, l, label='$l$')
-    ax1.plot(t, delta / 2, label='$\delta/2$')
-    ax1.plot(t, c, label='$c$')
-    ax1.plot(t, p, label='$p$')
-    ax1.plot(t, g1 / 2, label='$g_1/2$')
-    ax1.plot(t, g2 / 2, label='$g_2/2$')
+    ax1.plot(t, l, label='$l$', color=colors[0])
+    ax1.plot(t, delta / 2, label='$\delta/2$', color=colors[1])
+    ax1.plot(t, c + l, label='$p$', color=colors[2])
+
+    ax1.plot(results0.index, results0['l'], color=colors[0],
+             linestyle='dashed')
+    ax1.plot(results0.index, results0['del/2'], color=colors[1],
+             linestyle='dashed')
+    ax1.plot(results0.index, results0['p'], color=colors[2],
+             linestyle='dashed')
+    #ax1.plot(t, p, label='$p$')
+    #ax1.plot(t, g1 / 2, label='$g_1/2$')
+    #ax1.plot(t, g2 / 2, label='$g_2/2$')
     ax1.set_ylabel('Fraction of bridges')
     ax1.legend()
 
-    labels = ['solid', 'gas', 'tar', 'meta', 'cross']
+    labels = ['char', 'light_gas', 'tar']
     for i, l in enumerate(labels):
-        ax2.plot(t, fract[:, i], label=l)
+        ax2.plot(t, fract[:, i], label=l, color=colors[i])
+        ax2.plot(results0.index, results0[l],
+                 color=colors[i], linestyle='dashed')
     ax2.legend()
     ax2.set_xlim([0, 0.05])
     ax2.set_xlabel('Time, s')
